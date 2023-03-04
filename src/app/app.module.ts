54d48f3fa46ddaae09ede6cfc708724790ea9236
khouloud <khouloudmhamdi7@gmail.com>
2023-03-04 14:28:08 +0100

    ajout des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AproposPageComponent } from './components/apropos-page/apropos-page.component';
+import { AproposSectionComponent } from './components/apropos-section/apropos-section.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { ContactSectionComponent } from './components/contact-section/contact-section.component';
+import { DisciplinesSectionComponent } from './components/disciplines-section/disciplines-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AproposPageComponent,
+    AproposSectionComponent,
+    ContactPageComponent,
+    ContactSectionComponent,
+    DisciplinesSectionComponent
   ],
   imports: [
     BrowserModule,
